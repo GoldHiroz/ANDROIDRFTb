a60b38cc6aaf9493f7e5c395b99e546c8cc2020d
alper061 <dj.peat@gmail.com>
2015-12-17 13:02:50 +0100

    Added user add endpoint

--- webapi/user.php
+++ webapi/user.php
@@ -5,9 +5,31 @@
 
 include "main.php";
 
+$muvelet =$_GET["muvelet"];
 
+if ($muvelet =="add")
+{
+if (empty($_GET["nev"]) || empty($_GET["email"]))
+{
+die ("ERROR");
+}
+$nev = $_GET["nev"];
+$email = $_GET["email"];
+$sql ="INSERT INTO felhasznalok VALUES ('', '$nev', '$email');";
+print $sql;
+print "<br>";
+if (mysqli_query($kapcsolat, $sql))
+{
+print "OK";
+}
+else
+{
+print "ERROR";
+print mysqli_error();
+}
 
-print ("ok");
+}
+
 
 // A végén bezárjuk a kapcsolatot
 mysqli_close($kapcsolat);

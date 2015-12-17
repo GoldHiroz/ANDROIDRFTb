405059091ccd207e38dd940ec380be9ef71da5ff
alper061 <dj.peat@gmail.com>
2015-12-17 01:23:57 +0100

    Added api config file

---
+++ webapi/config.php
@@ -0,0 +1,24 @@
+<?php
+// RFT beadandó
+// Mobil alkalmazás web api
+// Alper061
+
+// Konfig
+
+// SQL adatok
+// SQL felhasználónév:
+$sqluser ="rft";
+// SQL jelszó
+$sqlpass ="rft";
+//SQL host (általában localhost)
+$sqlhost ="localhost";
+// SQL adatbázis
+$sqldb ="rft";
+
+// Egyéb beállítások
+
+
+// Kapcsolódunk az adatbázishoz
+
+
+?>

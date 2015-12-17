79a412063e85cb93981754edac2857dfed033727
alper061 <dj.peat@gmail.com>
2015-12-17 13:26:21 +0100

    Added categori add endpoint

--- webapi/cat.php
+++ webapi/cat.php
@@ -1,5 +1,33 @@
 <?php
+// RFT beadandó
+// Mobil alkalmazás web api
+// Alper061
+
 include "main.php";
 
+$muvelet =$_GET["muvelet"];
 
+if ($muvelet =="add")
+{
+if (empty($_GET["nev"]) || empty($_GET["leiras"]))
+{
+die ("ERROR");
+}
+$nev = $_GET["nev"];
+$leiras = $_GET["leiras"];
+$sql ="INSERT INTO kategoriak VALUES ('', '$nev', '$leiras');";
+if (mysqli_query($kapcsolat, $sql))
+{
+print "OK";
+}
+else
+{
+print "ERROR";
+}
+}
+
+
+
+// A végén bezárjuk a kapcsolatot
+mysqli_close($kapcsolat);
 ?>
